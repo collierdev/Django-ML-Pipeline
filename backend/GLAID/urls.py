77091875496795a7e32508515bf681ba574c5acf
Collier Development <collierjw93@gmail.com>
2022-03-27 02:59:57 -0400

    backend-frontend-creation

--- backend/GLAID/urls.py
+++ backend/GLAID/urls.py
@@ -1,4 +1,4 @@
-"""deploy URL Configuration
+"""GRAIL URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
