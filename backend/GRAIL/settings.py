e6745da8aee741a368a98ac901f60cfe340cc8a7
Collier Development <collierjw93@gmail.com>
2022-04-04 02:18:57 -0400

    secret

--- backend/GRAIL/settings.py
+++ backend/GRAIL/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hk&qd2zp*b66=ax3xd^#-nxa#7mv=!7vj05b%*3b_(t93r*@dp'
+SECRET_KEY = os.getenv('secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
